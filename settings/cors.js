0a88a2682647e4bcda8e2fa9552ff88243fb56af
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-03-20 10:40:53 +0100

    fix: added domains to cors + date fix

--- settings/cors.js
+++ settings/cors.js
@@ -4,6 +4,8 @@ const corsConfig = {
 	origin: [
 		'http://127.0.0.1:3000',
 		'http://localhost:3000',
+		'https://bullfrog-destined-cardinal.ngrok-free.app/',
+		'https://in-infinitely-polecat.ngrok-free.app/',
 	],
 	optionsSuccessStatus: 200,
 	exposedHeaders: ['Content-Range', 'X-Content-Range'],
