2f6ebf33282d95710a51128bf0d2a41512ff7115
Mafusn <Magnus.peetz.holt@gmail.com>
2024-04-05 12:13:02 +0200

    Removal of commented code

--- routes/skiAreaRoutes.js
+++ routes/skiAreaRoutes.js
@@ -5,8 +5,6 @@ import checkParams from '../utils/checkParams.js';
 import getQuery from '../utils/getQuery.js';
 import axios from 'axios';
 import { overpassToGeoJson } from '../utils/dataFormatter.js';
-// import savePistesFromArea from '../data_generation/savePistesFromArea.js';
-//import getCurrentWeather from '../utils/getWeather.js';
 
 const router = express.Router();
 
@@ -40,12 +38,6 @@ async (req, res) => {
 	if (!geoJson?.data) {
 		return res.status(500).send(err.routeGeneration.overpassApiError);
 	}
-
-	// const weather = await getCurrentWeather();
-	// console.log(weather.current, weather.hourly.snow_depth, weather.hourly.visibility);
-
-	// TODO: Only run this function if the user has admin roles
-	// await savePistesFromArea(overpassToGeoJson(geoJson.data), skiArea);
 
 	return res.status(200).send({
 		skiArea: skiArea,
