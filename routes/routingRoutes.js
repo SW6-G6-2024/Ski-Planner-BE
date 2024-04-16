468c973979bf7a5e66a89e5907fc84bd53df8d12
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-04-16 11:22:54 +0200

    fix: Small readability improvement

--- routes/routingRoutes.js
+++ routes/routingRoutes.js
@@ -3,75 +3,61 @@ import express from 'express';
 import err from '../utils/errorCodes.js';
 // eslint-disable-next-line no-unused-vars
 import { isPoint } from '../utils/pointValidator.js';
-import SkiArea from '../models/SkiAreas.js';
 import checkParams from '../utils/checkParams.js';
 import getQuery from '../utils/getQuery.js';
 import env from '../config/keys.js';
+import { findSkiArea } from '../utils/queryInDB.js';
 
 const router = express.Router();
 
-router.post('/generate-route', 
-/**
- * POST request for generating shortest route between two points
- * @param {Express.Request} req request object
- * @param {Express.Response} res response object
- * @returns 
- */
-async (req, res) => {
-	const { start, end, skiArea } = req.body;
-	if (checkParams([{
-			name: 'start point',
-			value: start,
-			func: isPoint,
-			funcErr: err.routeGeneration.invalidPoint,
-		}, {
-			name: 'end point',
-			value: end,
-			func: isPoint,
-			funcErr: err.routeGeneration.invalidPoint,
-		}, {
-			name: 'skiArea',
-			value: skiArea,
-			id: true,
+router.post('/generate-route',
+	/**
+	 * POST request for generating shortest route between two points
+	 * @param {Express.Request} req request object
+	 * @param {Express.Response} res response object
+	 * @returns 
+	 */
+	async (req, res) => {
+		const { start, end, skiArea } = req.body;
+		if (checkInput(start, end, skiArea, res)) {
+			return;
 		}
-	], res)) {
-		return;
-	}
 
-	const skiAreaInstance = await SkiArea.findById(skiArea);
+		let skiAreaInstance
 
-	if (!skiAreaInstance)
-		return res.status(400).send(err.skiArea.notFound);
+		try {
+			skiAreaInstance = await findSkiArea(skiArea);
+		} catch {
+			return res.status(400).send(err.skiArea.notFound);
+		}
 
-	const bounds = skiAreaInstance.bounds;
+		const query = getQuery(skiAreaInstance.bounds);
 
-	const query = getQuery(bounds);
+		const apiRes = await axios.post('https://overpass-api.de/api/interpreter', query);
+		if (!apiRes?.data)
+			return res.status(500).send(err.routeGeneration.overpassApiError);
 
-	const apiRes = await axios.post('https://overpass-api.de/api/interpreter', query);
-	if (!apiRes?.data)
-		return res.status(500).send(err.routeGeneration.overpassApiError);
+		// Call the route generation service
+		let result;
+		try {
+			result = await axios.post(env.pathFindingUrl + '/generate-route', {
+				data: {
+					start: start,
+					end: end,
+					geoJson: apiRes.data,
+				}
+			});
+		} catch (error) {
+			return res.status(500).send(err.routeGeneration.routeGenerationError);
+		}
 
-	// Call the route generation service
-	let result;
-	try {
-		result = await axios.post(env.pathFindingUrl + '/generate-route', {
-			data: {
-				start: start,
-				end: end,
-				geoJson: apiRes.data,
-			}
-		});
-	} catch (error) {
-		return res.status(500).send(err.routeGeneration.routeGenerationError);
-	}
+		// Only pass the shortest route and not the step-by-step guide
+		if (checkResult(result?.data?.[0], res)) {
+			return;
+		}
 
-	// Only pass the shortest route and not the step-by-step guide
-	if (checkResult(result?.data?.[0], res)) {
-		return;
-	}
-
-	return res.status(200).send({ route: 'Dis way!', res: result.data });
-});
+		return res.status(200).send({ route: 'Dis way!', res: result.data });
+	});
 
 /**
  * This function checks if the result is valid or not (i.e. if data and 
@@ -82,15 +68,42 @@ async (req, res) => {
  * @returns Sends the appropriate response if the result is invalid
  */
 function checkResult(result, res) {
-	if(!result) {
+	if (!result) {
 		res.status(500).send(err.routeGeneration.routeGenerationError);
 		return true;
 	}
 
-	if(!result.features || !result.features.length) {
+	if (!result.features || !result.features.length) {
 		res.status(400).send(err.routeGeneration.noRouteFound);
 		return true;
 	}
+}
+
+/**
+ * This function checks if the input is valid or not (i.e. if start and end are valid points and skiArea is a valid ID) and sends the appropriate response if not.
+ * @param {node} start The start point of the route
+ * @param {node} end The end point of the route
+ * @param {String} skiArea The ID of the ski area
+ * @param {Express.Response} res The express response object
+ * @returns Sends the appropriate response if the input is invalid and returns true, otherwise returns false
+ */
+function checkInput(start, end, skiArea, res) {
+	return checkParams([{
+		name: 'start point',
+		value: start,
+		func: isPoint,
+		funcErr: err.routeGeneration.invalidPoint,
+	}, {
+		name: 'end point',
+		value: end,
+		func: isPoint,
+		funcErr: err.routeGeneration.invalidPoint,
+	}, {
+		name: 'skiArea',
+		value: skiArea,
+		id: true,
+	}
+	], res)
 }
 
 export default router;
---
+++ utils/queryInDB.js
@@ -0,0 +1,15 @@
+import SkiArea from '../models/SkiAreas.js';
+
+/**
+ * This function finds the ski area with the given ID
+ * @param {string} id The ID of the ski area
+ * @returns The ski area with the given ID
+ */
+async function findSkiArea(id) {
+	const skiArea = await SkiArea.findById(id);
+	if (!skiArea)
+		throw new Error('Ski area not found');
+	return skiArea;
+}
+
+export { findSkiArea }
