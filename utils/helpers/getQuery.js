23ef88d472647c2e440df23cb6fcca1a9e129d86
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-05-02 09:23:12 +0200

    feat: getQuery rework

--- utils/helpers/getQuery.js
+++ utils/helpers/getQuery.js
@@ -3,17 +3,68 @@
  * @param {Array<Number>} bounds - [minLon, minLat, maxLon, maxLat]
  * @returns {String} Overpass API query string
  */
-export default (bounds) => (
-	`
-[out:json];
-(
-  way["piste:type"="downhill"]["piste:difficulty"!="freeride"]["ref"!="(30)"]["piste:difficulty"]["ref"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]}); 
-  way["piste:type"="downhill"]["piste:difficulty"!="freeride"]["piste:difficulty"]["name"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});
-  way["piste:type"="downhill"]["piste:difficulty"!="freeride"]["piste:difficulty"]["piste:ref"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]}); 
-  way["piste:type"="downhill"]["piste:difficulty"!="freeride"]["piste:difficulty"]["piste:name"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});
-  // Fetch all ski lifts within the bounding box
-  way["aerialway"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});
-);
-out geom;
-	`
-);
+export default (bounds, settings) => {
+  const PISTE_DIFFICULTY_MAPPING = {
+    'Very easy piste': 'novice',
+    'Easy piste': 'easy',
+    'Medium piste': 'intermediate',
+    'Expert piste': ['advanced', 'expert']
+  };
+  
+  const LIFT_TYPE_MAPPING = {
+    'Button lift': ['drag_lift', 'platter'],
+    'Chair lift': 'chair_lift',
+    'Gondola lift': 'gondola',
+    'Lift': 't-bar'
+  };
+
+  if (settings === undefined) {
+    settings = {
+      'Very easy piste': true,
+      'Easy piste': true,
+      'Medium piste': true,
+      'Expert piste': true,
+      'Button lift': true,
+      'Chair lift': true,
+      'Gondola lift': true,
+      'Lift': true
+    };
+  }
+  
+  let query = `
+  [out:json];
+  (
+    `;
+    
+    // Add pistes based on settings
+    Object.entries(PISTE_DIFFICULTY_MAPPING).forEach(([key, value]) => {
+      if (settings[key]) {
+        if (Array.isArray(value)) {
+          value.forEach(val => {
+            query += `way["piste:type"="downhill"]["piste:difficulty"="${val}"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});\n`;
+          });
+        } else {
+          query += `way["piste:type"="downhill"]["piste:difficulty"="${value}"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});\n`;
+        }
+      }
+    });
+
+    // Add lifts based on settings
+    Object.entries(LIFT_TYPE_MAPPING).forEach(([key, value]) => {
+      if (settings[key]) {
+        if (Array.isArray(value)) {
+          value.forEach(val => {
+            query += `way["aerialway"="${val}"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});\n`;
+          });
+        } else {
+          query += `way["aerialway"="${value}"](${bounds[0]},${bounds[1]},${bounds[2]},${bounds[3]});\n`;
+        }
+      }
+    });
+
+    query += `
+  );
+  out geom;
+  `;
+  return query;
+};
